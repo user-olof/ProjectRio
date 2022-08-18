93df628115137af98b65b0ca190907bfacfd8d9d
Olof Thornell <olof.thornell@gmail.com>
2022-08-18 22:25:29 +0100

    generic classed-based views

--- bookings/views.py
+++ bookings/views.py
@@ -8,41 +8,16 @@ from django.shortcuts import render
 # from rest_framework.parsers import JSONParser
 from bookings.models import Member
 from bookings.serializers import MemberSerializer
-from rest_framework import mixins
 from rest_framework import generics
-# from django.http import Http404
-# from rest_framework.views import APIView
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from rest_framework.response import Response
 
-# @api_view(['GET', 'POST'])
-class MemberList(mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
 
+class MemberList(generics.ListCreateAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):     
-        return self.create(request, *args, **kwargs)
 
-class MemberDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Member.objects.all()
     serializer_class = MemberSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
