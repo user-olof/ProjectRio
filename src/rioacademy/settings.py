3ce159d858757f7ba78e5d1e55d3f4a3dc52fd0a
Olof Thornell <olof.thornell@gmail.com>
2022-10-05 15:29:06 +0100

    deleted two empty files

--- src/rioacademy/settings.py
+++ src/rioacademy/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# # Sets the project root to 'src'
+# PROJECT_ROOT = PROJECT_ROOT = os.path.dirname(__file__)
+# sys.path.insert(0, os.path.join(PROJECT_ROOT, 'src'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
