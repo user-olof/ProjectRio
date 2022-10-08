a6bf3aa6ecb697f77a4ffcd8c75bfad1b3133c61
Olof Thornell <olof.thornell@gmail.com>
2022-10-08 22:01:36 +0100

    test

--- src/events_and_classes/tests.py
+++ src/events_and_classes/tests.py
@@ -2,24 +2,24 @@ from django.test import TestCase
 from django.test import Client
  
 # Create your tests here.
-from .models import Member
+# from .models import Member
 
 
-class EventsAndClassesTests(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        c = Client()
-        response = c.post('/localhost/')
-        response.status_code        
+# class EventsAndClassesTests(TestCase):
+#     @classmethod
+#     def setUpTestData(cls):
+#         c = Client()
+#         response = c.post('/localhost/')
+#         response.status_code        
 
-    class GetOneMember(TestCase):
-        def setUp(self):
-            Member.objects.create(
-                title="Mrs",
-                first_name="Molly",
-                surname="Holmes",
-                level="Beginner",
-                last_login=""
-                )
+#     class GetOneMember(TestCase):
+#         def setUp(self):
+#             Member.objects.create(
+#                 title="Mrs",
+#                 first_name="Molly",
+#                 surname="Holmes",
+#                 level="Beginner",
+#                 last_login=""
+#                 )
 
     
